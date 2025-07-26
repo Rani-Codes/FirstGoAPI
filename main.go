210c52b08caefb345f91fc8fbcd7afb756f8ad56
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-07-26 13:46:57 -0700

    broken get

--- main.go
+++ main.go
@@ -6,12 +6,30 @@ import (
 	"net/http"
 )
 
+type Inventory struct {
+	id   int
+	item string
+}
+
+var InvList = []Inventory{
+	{id: 1, item: "banana"},
+	{id: 2, item: "apple"},
+	{id: 3, item: "pear"},
+}
+
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
+func (i *Inventory) GetAllItems(w http.ResponseWriter, r *http.Request) {
+	for j := 0; j < len(i.item); j++ {
+		fmt.Fprintf(w, "The items inside of the inventory: %s", i.item[j])
+	}
+}
+
 func main() {
 	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/items", GetAllItems)
 
 	fmt.Println("Server running at localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
